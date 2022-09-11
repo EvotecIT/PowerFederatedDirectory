92527647481293c31e54e293befbabf10632a9a9
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-09-11 12:32:22 +0200

    Add example

---
+++ Examples/GetSchema.ps1
@@ -0,0 +1,6 @@
+﻿Import-Module .\PowerFederatedDirectory.psd1 -Force
+
+$Schema = Get-FederatedDirectorySchema
+$Schema | Where-Object { $_.Name -eq 'User' } | Select-Object -ExpandProperty Attributes | Format-Table
+$Schema | Where-Object { $_.Name -eq 'EnterpriseUser' } | Select-Object -ExpandProperty Attributes | Format-Table
+$Schema | Where-Object { $_.Name -eq 'FederatedDirectoryUser' } | Select-Object -ExpandProperty Attributes | Format-Table
