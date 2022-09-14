a270843f923ce21219be77df969c61cc04ac58e3
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-09-14 20:02:37 +0200

    Small improvement to examples

--- Examples/SetUsers.ps1
+++ Examples/SetUsers.ps1
@@ -6,4 +6,4 @@ Connect-FederatedDirectory -Token $Token -Suppress
 
 #Set-FederatedDirectoryUser -Id '11105df0-31cf-11ed-ada4-2bbc677ce86d' -DisplayName 'New name' -FamilyName 'New namme' -EmailAddressHome 'test@evo.pl' -PhoneNumberHome '502469000' -Verbose -Custom01 'test123' -Action Update
 
-Set-FederatedDirectoryUser -SearchUserName 'BringFood@test.pl' -Verbose
+Set-FederatedDirectoryUser -SearchUserName 'BringFood@test.pl' -Verbose -WhatIf -Custom01 'test123'
