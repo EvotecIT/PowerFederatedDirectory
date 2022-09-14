bc87473774b1247b47f16d71b9c41e6d3cb12e8d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-09-14 20:02:30 +0200

    Small improvement to examples

--- Examples/AddUsers.ps1
+++ Examples/AddUsers.ps1
@@ -2,14 +2,13 @@
 
 $Token = Get-Content -Raw -LiteralPath 'C:\Support\Important\Password-FederatedDirectory.txt'
 
-Connect-FederatedDirectory -Token $Token #-Suppress
+Connect-FederatedDirectory -Token $Token -Suppress
 
 #Add-FederatedDirectoryUser -DirectoryID $DirectoryID -UserName 'test1@evotec.pl' -DisplayName 'test' -Verbose
 #Add-FederatedDirectoryUser -UserName 'test11111@test.pl' -DisplayName 'T est111' -Verbose #-UserType Employee
 #Add-FederatedDirectoryUser -UserName 'TestUser1@test.pl' -DisplayName 'TestUser' -Verbose
-
 #Add-FederatedDirectoryUser -UserName 'TestUser2@test.pl' -DisplayName 'TestUser' -Verbose -ManagerUserName 'TestUser1@test.pl'
 
 for ($i = 1; $i -le 10; $i++) {
-    Add-FederatedDirectoryUser -UserName "TestUserNew$i@test.pl" -DisplayName "TestUserNew$i" -ManagerDisplayName 'TestUser' -Suppress
+    Add-FederatedDirectoryUser -UserName "TestUserNew$i@test.pl" -DisplayName "TestUserNew$i" -ManagerDisplayName 'TestUser' -Suppress -WhatIf
 }
