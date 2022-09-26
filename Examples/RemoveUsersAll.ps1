eb23e476b6218cb3643ec2d417a47513d2901e6f
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-09-26 22:22:27 +0200

    Add example to remove all users

---
+++ Examples/RemoveUsersAll.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module .\PowerFederatedDirectory.psd1 -Force
+
+$Token = Get-Content -Raw -LiteralPath 'C:\Support\Important\Password-FederatedDirectory.txt'
+
+Connect-FederatedDirectory -Token $Token -Suppress
+
+Remove-FederatedDirectoryUser -All -Verbose -WhatIf
